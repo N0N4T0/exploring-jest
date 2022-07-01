a633ec6dd6bcab8e6d1dd99070c86181984508fb
LUCIO Notebook <luciononato@hotmail.com>
2022-07-01 07:52:10 -0300

    feat(): testing hello world

---
+++ react-unit-testing/src/App.spec.tsx
@@ -0,0 +1,9 @@
+import {render} from '@testing-library/react'
+import App from './App'
+// render permite importar o componente 
+
+test('component App', ()=> {
+    const {getByText} = render(<App/>)
+
+    expect(getByText(/hello world/i)).toBeTruthy()
+})
