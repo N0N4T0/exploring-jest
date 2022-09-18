015adc9a6ac66b4e8585cadbfa1646dad18657ae
LUCIO Notebook <luciononato@hotmail.com>
2022-09-18 12:37:40 -0300

    feat: config snapshot test

---
+++ sorteador-de-amigo-secreto/src/paginas/Configuracao.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { RecoilRoot } from 'recoil'
+import { Configuracao } from './Configuracao'
+
+const mockNavegacao = jest.fn()
+
+jest.mock('react-router-dom', () => {
+  return {
+    useNavigate: () => mockNavegacao
+  }
+})
+
+describe('a pagina de configuracao', () => {
+
+  test('deve ser renderizada corretamente', () => {
+    const { container } = render(
+      <RecoilRoot>
+        <Configuracao />
+      </RecoilRoot>
+    )
+
+    //teste de snapshot: a primeira vez que rodamos a foto é tirada
+    // nas demais vezes a foto é comparada.
+    expect(container).toMatchSnapshot()
+  })
+})
