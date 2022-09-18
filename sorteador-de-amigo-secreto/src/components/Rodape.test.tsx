0af7b717e47b7105696d0a9516b12285a9517c7d
LUCIO Notebook <luciononato@hotmail.com>
2022-09-17 22:22:19 -0300

    feat: add test navigtioin between routes

--- sorteador-de-amigo-secreto/src/components/Rodape.test.tsx
+++ sorteador-de-amigo-secreto/src/components/Rodape.test.tsx
@@ -1,8 +1,27 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { RecoilRoot } from "recoil"
+import { useListaDeParticipantes } from "../state/hook"
 import { Rodape } from "./Rodape"
 
-describe('onde não existe participantes suficientes', () => {
+jest.mock('../state/hook/useListaDeParticipantes', () => {
+  return {
+    useListaDeParticipantes: jest.fn()
+  }
+})
+
+const mockNavegacao = jest.fn()
+
+jest.mock('react-router-dom', () => {
+  return {
+    useNavigate: () => mockNavegacao
+  }
+})
+
+describe('quando não existe participantes suficientes', () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue([])
+  })
+
   test('a brincadeira não pode ser iniciada', () => {
     render(
       <RecoilRoot>
@@ -13,5 +32,37 @@ describe('onde não existe participantes suficientes', () => {
     const botao = screen.getByRole('button')
 
     expect(botao).toBeDisabled()
+  })
+})
+
+describe('quando existem participantes suficientes', () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue(['Ana', 'Catarina', 'Josef'])
+  })
+
+  test('a brincadeira pode ser iniciada', () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>
+    )
+
+    const botao = screen.getByRole('button')
+
+    expect(botao).not.toBeDisabled()
+  })
+
+  test('a brincadeira foi iniciada', () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>
+    )
+
+    const botao = screen.getByRole('button')
+    fireEvent.click(botao)
+
+    expect(mockNavegacao).toHaveBeenCalledTimes(1)
+    expect(mockNavegacao).toHaveBeenCalledWith('/sorteio')
   })
 })
--- sorteador-de-amigo-secreto/src/components/Rodape.tsx
+++ sorteador-de-amigo-secreto/src/components/Rodape.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom"
 import { useListaDeParticipantes } from "../state/hook"
 
 export function Rodape() {
   const participantes = useListaDeParticipantes()
 
+  const navegarPara = useNavigate()
+
+  const iniciar = () => {
+    navegarPara('/sorteio')
+  }
+
   return (
     <footer>
-      <button disabled={participantes.length < 3}>Iniciar bricadeira</button>
+      <button disabled={participantes.length < 3} onClick={iniciar}>Iniciar bricadeira</button>
     </footer>
   )
 }
