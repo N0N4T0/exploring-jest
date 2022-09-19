5689fa807ec0ec67f988beb41fba2d38d192076f
LUCIO Notebook <luciononato@hotmail.com>
2022-09-19 08:34:09 -0300

    feat: add lib shuffle, add business rule in function to be testable

---
+++ sorteador-de-amigo-secreto/src/state/helpers/realizarSorteio.ts
@@ -0,0 +1,18 @@
+import shuffle from "just-shuffle"
+
+//regra de negócio na função pura, pois na função pura nós podemos testar
+
+export function realizarSorteio(participantes: string[]) {
+  const totalDeParticipantes = participantes.length
+  const embaralhado = shuffle(participantes)
+
+  const resultado = new Map<string, string>()
+
+  for (let index = 0; index < totalDeParticipantes; index++) {
+    const indiceDoAmigo = index === (totalDeParticipantes - 1) ? 0 : index + 1
+
+    resultado.set(embaralhado[index], embaralhado[indiceDoAmigo])
+  }
+
+  return resultado
+}
