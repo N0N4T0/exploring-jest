4a764adc69fe3cd769b1ce17c3429151e067b153
LUCIO Notebook <luciononato@hotmail.com>
2022-09-25 09:57:25 -0300

    feat: finish prie draw page

--- sorteador-de-amigo-secreto/src/paginas/Sorteio.tsx
+++ sorteador-de-amigo-secreto/src/paginas/Sorteio.tsx
@@ -1,7 +1,24 @@
+import React from "react"
+import Card from "../components/Card"
 import { useListaDeParticipantes } from "../state/hook"
+import { useResultadoSorteio } from "../state/hook/useResultadoSorteio"
+
+import "./Sorteio.css"
 
 export const Sorteio = () => {
   const participantes = useListaDeParticipantes()
+
+  const [participanteDaVez, setParticipanteDaVez] = React.useState('')
+  const [amigoSecreto, setAmigoSecreto] = React.useState('')
+
+  const resultado = useResultadoSorteio()
+
+  const sortear = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    if (resultado.has(participanteDaVez)) {
+      setAmigoSecreto(resultado.get(participanteDaVez)!)
+    }
+  }
 
   function renderAllParticipantes() {
     return (
@@ -14,12 +31,29 @@ export const Sorteio = () => {
   }
 
   return (
-    <section>
-      <form>
-        <select name="participanteDaVez" id="participanteDaVez">
-          {renderAllParticipantes()}
-        </select>
-      </form>
-    </section>
+    <Card>
+      <section className="sorteio">
+        <form onSubmit={sortear}>
+          <select
+            required
+            name="participanteDaVez"
+            id="participanteDaVez"
+            placeholder="Selecione o seu nome"
+            value={participanteDaVez}
+            onChange={evento => setParticipanteDaVez(evento.target.value)}
+          >
+            {renderAllParticipantes()}
+          </select>
+          <p> Clique em sortear para ver quem é seu amigo secreto!</p>
+          <button>Sortear</button>
+        </form>
+        {amigoSecreto && <p role='alert'>{amigoSecreto}</p>}
+
+        <footer className="sorteio">
+          <img src="/imagens/aviao.png" className="aviao" alt="Um desenho de um avião de papel" />
+        </footer>
+      </section>
+    </Card>
+
   )
 }
