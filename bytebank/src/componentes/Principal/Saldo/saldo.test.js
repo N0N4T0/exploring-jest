62343fda8224ae6ff11351aa428543333d94be77
Lucio <luciononato@hotmail.com>
2023-01-19 00:15:18 -0300

    test: add tests on Saldo component

---
+++ bytebank/src/componentes/Principal/Saldo/saldo.test.js
@@ -0,0 +1,12 @@
+const { render, screen } = require("@testing-library/react")
+
+import Saldo from './index'
+
+describe('component Saldo', () => {
+  test('Deve renderizar o saldo com valor monetário', () => {
+    render(<Saldo saldo={1000}/>)
+
+    const saldo = screen.getByTestId('saldo')
+    expect(saldo).toHaveTextContent('R$ 1000')
+  })
+})
